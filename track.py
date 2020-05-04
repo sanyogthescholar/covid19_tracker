54649d4b3838f9f30f75b52c2831c0b0d83c599a
Sanyog Jain <sanyogjain5678@gmail.com>
2020-05-04 17:34:39 +0530

    Add comments

--- track.py
+++ track.py
@@ -2,15 +2,14 @@ import requests
 import json
 from twython import Twython
 
-C_key = ""
-C_secret = ""
-A_token = ""
-A_secret = ""
+C_key = ""#Your consumer API key
+C_secret = ""#Your consumer secret key
+A_token = ""#Your access token key
+A_secret = ""#Your access token secret key
 
 response = requests.get("https://api.covid19api.com/summary")
 def run(obj):
-    dict1=obj.get('Global')
-    #print(dict1)
+    dict1=obj.get('Global')#Data is received as a Python Dictionary
     tf='New confimed cases:'
     tf+=str(dict1.get('NewConfirmed'))
     tf+='\nNew deaths:'
